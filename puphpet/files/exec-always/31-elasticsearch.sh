f0b6b947122544b298233597c759152a70c7fecc
Roman Allenstein <r.allenstein@reply.de>
2016-02-06 13:42:28 +0100

    set fixed port for elasticsearch

--- puphpet/files/exec-always/31-elasticsearch.sh
+++ puphpet/files/exec-always/31-elasticsearch.sh
@@ -3,6 +3,9 @@ FILE=/etc/elasticsearch/es-01/elasticsearch.yml
 LINE="network.bind_host: 0"
 grep -q "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 
+LINE="http.port: 9200"
+grep -q "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+
 LINE="http.cors.enabled : true"
 grep -q "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 
@@ -14,3 +17,4 @@ grep -q "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 
 LINE="http.cors.allow-origin : '*'"
 grep -q "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+
