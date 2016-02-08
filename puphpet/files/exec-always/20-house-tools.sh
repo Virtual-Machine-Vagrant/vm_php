6bcb45fb04258def32b5a40801bb1f45d9fe445a
Roman Allenstein <r.allenstein@reply.de>
2016-02-08 19:04:17 +0100

    moved tools to /tools

--- puphpet/files/exec-always/20-house-tools.sh
+++ puphpet/files/exec-always/20-house-tools.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 rm -rf /var/www/html
 HOUSE_PATH=/var/www/house.local
+HOUSE_TOOLS_PATH=/var/www/house.local/tools
+mkdir -p $HOUSE_TOOLS_PATH
 echo "---------------------------------------------"
-echo "Installtion tools into $HOUSE_PATH"
+echo "Installtion tools into $HOUSE_TOOLS_PATH"
 echo "---------------------------------------------"
 
 echo "---------------------------------------------"
 echo "Elasticsearch: Installing elasticsearch HQ"
 echo "---------------------------------------------"
-cd $HOUSE_PATH
+cd $HOUSE_TOOLS_PATH
 git clone https://github.com/royrusso/elasticsearch-HQ.git
 
 echo "---------------------------------------------"
 echo "MongoDB: Installing genghis"
 echo "---------------------------------------------"
-GENGHIS_PATH=$HOUSE_PATH/genghis
+GENGHIS_PATH=$HOUSE_TOOLS_PATH/genghis
 mkdir -p $GENGHIS_PATH
 cd $GENGHIS_PATH
 wget https://raw.githubusercontent.com/bobthecow/genghis/master/genghis.php
@@ -22,7 +24,7 @@ wget https://raw.githubusercontent.com/bobthecow/genghis/master/genghis.php
 echo "---------------------------------------------"
 echo "MySQL: Installing adminer"
 echo "---------------------------------------------"
-ADMINER_PATH=$HOUSE_PATH/adminer
+ADMINER_PATH=$HOUSE_TOOLS_PATH/adminer
 mkdir -p $ADMINER_PATH
 cd $ADMINER_PATH
 wget https://www.adminer.org/latest-mysql.php
