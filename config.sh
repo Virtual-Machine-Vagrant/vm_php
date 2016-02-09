b01281ba7fc45696fcf3ff136df53f219b8a0601
Roman Allenstein <r.allenstein@reply.de>
2016-02-09 13:12:38 +0100

    a lot of changes

--- config.sh
+++ config.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # house.local config file
-export HOUSE_PHP_VERSIONS=(5.3.29 5.4.45)		# separated by space, the first version will be the default version
+export HOUSE_PHP_VERSIONS=(5.3.29)		# separated by space, the first version will be the default version
+# 5.3.29 5.4.45 5.5.31 
 export HOUSE_PHP_ACTIVE_VERSION=${HOUSE_PHP_VERSIONS[0]}
