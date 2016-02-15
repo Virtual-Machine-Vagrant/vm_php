97333900a7a8708e8956abdfd0d6640b4c2f264f
Roman Allenstein <r.allenstein@reply.de>
2016-02-15 13:44:36 +0100

    added composer parallel plugin

--- puphpet/files/exec-always/02-composer-update.sh
+++ puphpet/files/exec-always/02-composer-update.sh
@@ -3,3 +3,7 @@ echo "---------------------------------------------"
 echo "Updating composer"
 echo "---------------------------------------------"
 composer self-update
+echo "---------------------------------------------"
+echo "Installing composer parallel plugin"
+echo "---------------------------------------------"
+composer global require hirak/prestissimo
